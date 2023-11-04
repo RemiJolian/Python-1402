5cb02ad546f0fb887919c881247dddc38cf4140c
Ramin <rbagheri85@gmail.com>
2023-11-04 10:45:37 +0330

    examples in use of super() func, and import module

---
+++ New_Test_1402/Review_Python/example.py
@@ -0,0 +1,5 @@
+#This file is a test, and it is used in another file
+def add(a, b):
+    result = a + b
+    return result
+#print(add(2,3))
---
+++ New_Test_1402/Review_Python/example2.py
@@ -0,0 +1,9 @@
+# Python Module
+def add(a, b):
+    result = a + b
+    return result
+
+if __name__ == "__main__":
+    print("This is the main program.")
+    result = add(2, 3)
+    print(result)

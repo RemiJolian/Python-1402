49984429086075c3d17546abea88f65ba924cb20
Ramin <rbagheri85@gmail.com>
2023-11-20 22:36:31 +0330

    socket prog practice

---
+++ New_Test_1402/Review_Python/snake game using socket/snake.py
@@ -0,0 +1,16 @@
+import pygame, consts, sys
+
+
+class Snake:
+
+    def __init__(self, keys, game, pos, color, direction):
+        pass
+
+    def get_head(self):
+        pass
+
+    def next_move(self):
+        pass
+
+    def handle(self, keys):
+        pass

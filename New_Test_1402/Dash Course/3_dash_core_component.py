e9bccfe04e0f1aa74086eb95c769fa7c3040fd97
Ramin <rbagheri85@gmail.com>
2024-01-07 11:27:01 +0330

    dash continues

--- New_Test_1402/Dash Course/3_dash_core_component.py
+++ New_Test_1402/Dash Course/3_dash_core_component.py
@@ -4,6 +4,7 @@ import plotly.express as px
 
 happiness = pd.read_csv('Dash Course/world_happiness.csv')
 line_fig = px.line(happiness[happiness['country']=='United States'],
+                               #see below comment   
                                x = 'year', y = 'happiness_score',
                                title = 'Happiness Score in the USA')
 
@@ -17,6 +18,7 @@ app.layout = html.Div([
                    href = 'https://worldhappiness.report',
                    target = '_blank')]),
     dcc.RadioItems(options = happiness['region'].unique(),
+                   # see below comment
                    value = 'North America'),
     dcc.Checklist(options = happiness['region'].unique(),
                    value = ['North America']),
@@ -28,3 +30,12 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     app.run_server(debug = True)  
+
+# in dcc.RadioItems(), options is set to happiness['region'].unique(), which 
+# returns an array of unique values of the 'region' column from the 'happiness' dataframe.
+# These unique values are then used as the options for the dcc.RadioItems component.
+    
+#The line happiness[happiness['country']=='United States'] is used to filter the
+# 'happiness' dataframe to only include rows where the 'country' column is equal to
+# 'United States'. This is a common operation in pandas, where you can filter a DataFrame based
+# on a certain condition.
--- New_Test_1402/Dash Course/4- building_simple_callback.py
+++ New_Test_1402/Dash Course/4- building_simple_callback.py
@@ -3,29 +3,30 @@ from dash import Dash, html, dcc, Input, Output
 
 app = Dash()
 # first way:
-#app.layout = html.Div([
-#     dcc.Input(id='input-text',value='Change this text',type='text'),
-#     html.Div(id = 'output-text')
-# ])
+app.layout = html.Div([
+    dcc.Input(id='input-text',value='Change this text',type='text'),
+    html.Div(id = 'output-text')
+])
 
 #second way:
 
-input_text = dcc.Input(value='Change this text',type='text')
-output_text = html.Div()
+# input_text = dcc.Input(value='Change this text',type='text')
+# output_text = html.Div()
 
-app.layout = html.Div([input_text, output_text])
+# app.layout = html.Div([input_text, output_text])
 
 @app.callback(
     # First way:
-    # Output(component_id = 'output-text', component_property = 'children'),
-    # Input(component_id = 'input-text', component_property = 'value')
+    Output(component_id = 'output-text', component_property = 'children'),
+    Input(component_id = 'input-text', component_property = 'value')
     # second:
-    Output(component_id = output_text, component_property = 'children'),
-    Input(component_id = input_text, component_property = 'value')
+    # Output(component_id = output_text, component_property = 'children'),
+    # Input(component_id = input_text, component_property = 'value')
 )
 
 def update_output_div(input_text):
     return f'Text : {input_text}'
+
 
 if __name__ == '__main__':
     app.run_server(debug = True) 
